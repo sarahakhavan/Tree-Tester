fd3b1ea02ff3fa8d25d54cb944c85241234101e1
Sarah <sarah.9473@gmail.com>
2023-01-14 22:21:52 -0500

    Initial Commit

---
+++ Tree Tester/Tree_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tree_TesterApp.swift
+//  Tree Tester
+//
+//  Created by Sarah Akhavan on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct Tree_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
